3acdf7891a35e043aa688647f2fbf06631b8803c
nicsell88 <pippim@gmail.com>
2022-09-16 14:42:17 +1200

    commented out unused Routes in app.js so the app runs

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import NavBar from './components/ResultsComponents/NavBar'
 import ListingsTable from './pages/Results/ListingsTable'
-import Landing from './pages/Landing/Landing.jsx';
-import Apply from './pages/Apply/Apply.jsx';
-import Enquire from './pages/Enquire/Enquire.jsx';
+// import Landing from './pages/Landing/Landing.jsx';
+// import Apply from './pages/Apply/Apply.jsx';
+// import Enquire from './pages/Enquire/Enquire.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -17,10 +17,7 @@ function App() {
           <BrowserRouter>
               <NavBar/>
               <Routes>
-                  <Route path='/' element={ <Landing/> } />
                   <Route path='/listings/list' element={ <ListingsTable/> } />
-                  <Route path='/apply' element={ <Apply/> } />
-                  <Route path='/enquire' element={ <Enquire/> } />
               </Routes>
           </BrowserRouter>
       </div>
@@ -28,3 +25,21 @@ function App() {
 }
 
 export default App
+
+// function App() {
+//     return (
+//       <div className="App">
+//           <BrowserRouter>
+//               <NavBar/>
+//               <Routes>
+//                   <Route path='/' element={ <Landing/> } />
+//                   <Route path='/listings/list' element={ <ListingsTable/> } />
+//                   <Route path='/apply' element={ <Apply/> } />
+//                   <Route path='/enquire' element={ <Enquire/> } />
+//               </Routes>
+//           </BrowserRouter>
+//       </div>
+//     );
+// }
+
+// export default App
