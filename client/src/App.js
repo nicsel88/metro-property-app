3414b0de856777c34e8e9c3cdbf9b7916e3cd3e8
nicsell88 <pippim@gmail.com>
2022-09-16 14:29:21 +1200

    added routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import { useState } from 'react'
 import NavBar from './components/ResultsComponents/NavBar'
 import ListingsTable from './pages/Results/ListingsTable'
+import Landing from './pages/Landing/Landing.jsx';
+import Apply from './pages/Apply/Apply.jsx';
+import Enquire from './pages/Enquire/Enquire.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css';
 
 function App() {
     return (
@@ -12,7 +17,10 @@ function App() {
           <BrowserRouter>
               <NavBar/>
               <Routes>
+                  <Route path='/' element={ <Landing/> } />
                   <Route path='/listings/list' element={ <ListingsTable/> } />
+                  <Route path='/apply' element={ <Apply/> } />
+                  <Route path='/enquire' element={ <Enquire/> } />
               </Routes>
           </BrowserRouter>
       </div>
